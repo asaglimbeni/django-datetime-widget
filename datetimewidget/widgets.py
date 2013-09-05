31095a2b86460861d74a94df870f5d2971623402
Alfredo Saglimbeni <a.saglimbeni@scsitaly.com>
2013-09-05 18:41:11 +0200

    Add localization support for datetime format FIX #14

--- datetimewidget/widgets.py
+++ datetimewidget/widgets.py
@@ -7,7 +7,6 @@ import uuid
 from django.forms.widgets import  MultiWidget , to_current_timezone, DateTimeInput
 from datetime import datetime
 from django.utils.formats import get_format, get_language
-
 I18N = """
 $.fn.datetimepicker.dates['en'] = {
     days: %s,
@@ -40,22 +39,48 @@ datetimepicker_options = """
     language : '%s',
 """
 
-dateConversion = {
-    'P' : '%p',
-    'ss' : '%S',
-    'ii' : '%M',
-    'hh' : '%H',
-    'HH' : '%I',
-    'dd' : '%d',
-    'mm' : '%m',
-    #'M' :  '%b',
-    #'MM' : '%B',
-    'yy' : '%y',
-    'yyyy' : '%Y',
+dateConversiontoPython = {
+    'P': '%p',
+    'ss': '%S',
+    'ii': '%M',
+    'hh': '%H',
+    'HH': '%I',
+    'dd': '%d',
+    'mm': '%m',
+    #'M':  '%b',
+    #'MM': '%B',
+    'yy': '%y',
+    'yyyy': '%Y',
 }
+
+dateConversiontoJavascript = {
+    '%M': 'ii',
+    '%m': 'mm',
+    '%I': 'HH',
+    '%H': 'hh',
+    '%d': 'dd',
+    '%Y': 'yyyy',
+    '%y': 'yy',
+    '%p': 'P',
+    '%S': 'ss'
+}
+
 class DateTimeWidget(MultiWidget):
 
-    def __init__(self, attrs=None, options=None):
+    def to_local(self):
+        """
+        This method have to be called on every request call, because adapt the datetime format to the user.
+        !!! It work only if USE_L10N is set TRUE and localize middleware is active.!!!
+        otherwise get_format use the server format.
+        """
+        pattern = re.compile(r'(?<!\w)(' + '|'.join(dateConversiontoJavascript.keys()) + r')\b')
+        self.format = get_format('DATETIME_INPUT_FORMATS')[0]
+        if hasattr(self, 'widgets') and self.widgets[0]:
+            self.widgets[0].format = self.format
+        self.option = (pattern.sub(lambda x: dateConversiontoJavascript[x.group()], self.format),) + self.option[1:]
+        self.language = get_language()
+
+    def __init__(self, attrs=None, options=None, usel10n = None):
         if attrs is None:
             attrs = {'readonly':''}
 
@@ -63,12 +88,20 @@ class DateTimeWidget(MultiWidget):
             options = {}
 
         self.option = ()
-        self.option += (options.get('format','dd/mm/yyyy hh:ii'),)
+        if usel10n is True:
+            self.is_localized = True
+            #Use local datetime format Only if USE_L10N is true and middleware localize is active
+            self.to_local()
+        else:
+            pattern = re.compile(r'\b(' + '|'.join(dateConversiontoPython.keys()) + r')\b')
+            self.option += (options.get('format','dd/mm/yyyy hh:ii'),)
+            self.format = pattern.sub(lambda x: dateConversiontoPython[x.group()], self.option[0])
+
         self.option += (options.get('startDate',''),)
         self.option += (options.get('endDate',''),)
         self.option += (options.get('weekStart','0'),)
         self.option += (options.get('daysOfWeekDisabled','[]'),)
-        self.option += (options.get('autoclose','false'),)
+        self.option += (options.get('autoclose','true'),)
         self.option += (options.get('startView','2'),)
         self.option += (options.get('minView','0'),)
         self.option += (options.get('maxView','4'),)
@@ -82,15 +115,16 @@ class DateTimeWidget(MultiWidget):
         self.language = options.get('language', 'en')
         self.option += (self.language,)
 
-        pattern = re.compile(r'\b(' + '|'.join(dateConversion.keys()) + r')\b')
-        self.dataTimeFormat = self.option[0]
-        self.format =  pattern.sub(lambda x: dateConversion[x.group()], self.option[0])
-
-        widgets = (DateTimeInput(attrs=attrs,format=self.format),)
+        widgets = (DateTimeInput(attrs=attrs, format=self.format),)
 
         super(DateTimeWidget, self).__init__(widgets, attrs)
 
     def value_from_datadict(self, data, files, name):
+
+        if self.is_localized:
+            #Adapt the format to the user.
+            self.to_local()
+
         date_time = [
             widget.value_from_datadict(data, files, name + '_%s' % i)
             for i, widget in enumerate(self.widgets)
@@ -103,8 +137,13 @@ class DateTimeWidget(MultiWidget):
             return D
 
     def decompress(self, value):
+
+        if self.is_localized:
+            #Adapt the format to the user.
+            self.to_local()
+
         if value:
-            value = to_current_timezone(value)
+            value = to_current_timezone(value).strftime(self.format)
             return (value,)
         return (None,)
 
@@ -116,7 +155,12 @@ class DateTimeWidget(MultiWidget):
         Returns a Unicode string representing the HTML for the whole lot.
         """
 
+        if self.is_localized:
+            #Adapt the format to the user.
+            self.to_local()
+
         js_options = datetimepicker_options % self.option
+
         id = uuid.uuid4().hex
         return '<div id="%s"  class="input-append date form_datetime">'\
                '%s'\
@@ -133,7 +177,7 @@ class DateTimeWidget(MultiWidget):
 
         return widgets.Media(
             css={
-                'all' : ('css/datetimepicker.css',)
+                'all': ('css/datetimepicker.css',)
             },
             js=js
         )
