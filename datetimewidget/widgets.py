29e632a97a541d2252759bf847262e27841b02f7
Lucas Clemente Vella <lvella@gmail.com>
2013-09-12 16:27:26 -0300

    Fixing syntax error when installing on Python 3.2.

--- datetimewidget/widgets.py
+++ datetimewidget/widgets.py
@@ -131,7 +131,7 @@ class DateTimeWidget(MultiWidget):
         ]
         try:
             D = to_current_timezone(datetime.strptime(date_time[0], self.format))
-        except (ValueError, TypeError), e:
+        except (ValueError, TypeError):
             return ''
         else:
             return D
