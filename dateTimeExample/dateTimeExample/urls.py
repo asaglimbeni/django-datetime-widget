5c350b3719f61d4b7f9cc58c3486245be74bb3ae
Alfredo Saglimbeni <a.saglimbeni@scsitaly.com>
2014-07-30 16:44:29 +0200

    redirect to default page in example

--- dateTimeExample/dateTimeExample/urls.py
+++ dateTimeExample/dateTimeExample/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import  settings
+from django.views.generic import RedirectView
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -17,4 +18,5 @@ urlpatterns = patterns('',
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.STATIC_ROOT}),
     ("^", include("formexample.urls")),
+    (r'^.*$', RedirectView.as_view(url='/model_form_v3/')),
 )
