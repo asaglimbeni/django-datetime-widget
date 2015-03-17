087339acf41b58d2305ec177869025fd1c66f31a
pdt <thanh.phan@outlook.com>
2015-03-17 20:22:17 +0100

    add README and requirements. also fixed settings to get rid of the warning for improper unittest settings

--- dateTimeExample/dateTimeExample/settings.py
+++ dateTimeExample/dateTimeExample/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'testDateTimeWidget',                      # Or path to database file if using sqlite3.
+        'NAME': 'testDateTimeWidget.sql',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -157,3 +157,6 @@ LOGGING = {
         },
     }
 }
+
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
