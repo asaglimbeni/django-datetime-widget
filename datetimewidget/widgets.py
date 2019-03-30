ca79bb8beca00865e3958c6cba8708a45a73f4fc
Erwin Gelissen <erwin.gelissen@gmail.com>
2019-03-30 19:26:14 +0100

    Bugfix

--- datetimewidget/widgets.py
+++ datetimewidget/widgets.py
@@ -251,15 +251,15 @@ class PickerWidgetMixin(object):
 
     def _media(self):
 
-        js = ["js/bootstrap-datetimepicker.js"]
+        js = ["datetimewidget/js/bootstrap-datetimepicker.js"]
 
         language = self.options.get('language', 'en')
         if language != 'en':
-            js.append("js/locales/bootstrap-datetimepicker.%s.js" % language)
+            js.append("datetimewidget/js/locales/bootstrap-datetimepicker.%s.js" % language)
 
         return widgets.Media(
             css={
-                'all': ('css/datetimepicker.css',)
+                'all': ('datetimewidget/css/datetimepicker.css',)
                 },
             js=js
             )
