a6a4c75bcb7230512da5cd8845a973b8555e835f
suresh <you@example.com>
2020-10-19 17:36:39 +0530

    django22

--- datetimewidget/widgets.py
+++ datetimewidget/widgets.py
@@ -223,7 +223,7 @@ class PickerWidgetMixin(object):
 
         super(PickerWidgetMixin, self).__init__(attrs, format=self.format)
 
-    def render(self, name, value, attrs=None):
+    def render(self, name, value, renderer=None, attrs=None):
         final_attrs = self.build_attrs(self.attrs, attrs)
         rendered_widget = super(PickerWidgetMixin, self).render(name, value, final_attrs)
 
